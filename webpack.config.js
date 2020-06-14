79715ecf46191a49fabfbea8ac47a125cd983e94
amarbalu <inboxamarbalu@gmail.com>
2020-06-14 08:29:07 +0530

    webpack config installation,with complete js setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path =require("path");
+const HtmlWebpackPlugin=require("html-webpack-plugin");
 module.exports={
     entry:path.resolve(__dirname,"src","app","app.js"),
     output:{
         filename:"main.js",
         path:path.resolve(__dirname,"build")
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:path.resolve(__dirname,"index.html")
+        })
+    ]
 }
